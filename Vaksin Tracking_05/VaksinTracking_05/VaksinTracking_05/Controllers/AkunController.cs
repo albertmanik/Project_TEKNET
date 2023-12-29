4513369444705d96ee8aa2a6f75837734dcbf632
albertmanik <albertmanik2808@gmail.com>
2023-12-29 16:53:54 +0700

    first commit

---
+++ Vaksin Tracking_05/VaksinTracking_05/VaksinTracking_05/Controllers/AkunController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using VaksinTracking_05.Models;
+
+namespace VaksinTracking_05.Controllers
+{
+    public class AkunController : Controller
+    {
+        Vaksin_Entities db = new Vaksin_Entities();
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Akun akun)
+        {
+            using (Vaksin_Entities context = new Vaksin_Entities())
+            {
+                bool IsValidUser = context.Akuns.Any(x => x.username.ToLower() ==
+                     akun.username.ToLower() && x.password == akun.password);
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(akun.username, false);
+                    Response.Write("<script>alert('Anda Login Sebagai')</script>");
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Username atau Pasword anda salah!!')</script>");
+                }
+                return View();
+            }
+
+        }
+    }
+}
---
+++ Vaksin Tracking_05/VaksinTracking_05/VaksinTracking_05/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VaksinTracking_05.MvcApplication" Language="C#" %>
